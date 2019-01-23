3d321330ea716680a66e7a6f6057729b8cb8c8f1
Agustin Meriles <jameriles@gmail.com>
2019-01-23 12:40:03 -0300

    Layout/Mock de página principal de la aplicación

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="nav-bar-top">
+          Mi Ciudad
+        </div>
+        <div className="container">
+          <div className="weather-location">
+            <div className="weather-data">
+              <div className="weather-data-icon">
+                Icono
+              </div>
+              <div className="weather-data-info">
+                <h2>25ºC</h2>
+                <div>
+                  <h4>Min: 10ºC</h4>
+                  <h4>Max: 30ºC</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
