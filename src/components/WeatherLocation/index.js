12ffa04fe783ca7d3e01c4d80f1fac67f7722621
Agustin Meriles <jameriles@gmail.com>
2019-02-16 23:18:04 -0300

    fixes

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import WeatherData from './WeatherData/index.js';
 import './style.css';
 
-const WeatherLocation = ({city}) => (
+const WeatherLocation = ({ city }) => (
     <div className="weather-location">
         <h1>{city}</h1>
         <WeatherData />
